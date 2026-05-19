58f608151098958252564a25158b5e1706644f96
melinya-byte <melinya.mir@gmail.com>
2026-05-19 16:54:50 -0300

    Update TarjetaIntegrante.jsx

--- src/components/TarjetaIntegrante.jsx
+++ src/components/TarjetaIntegrante.jsx
@@ -1,1 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+const TarjetaIntegrante = ({ name, avatar, profilePath }) => {
+  return (
+    <article className="tarjeta-item">
+      <div className="avatar-container">
+        <img src={avatar} alt={name} />
+      </div>
+      <h3>{name}</h3>
+      <Link to={profilePath} className="btn-perfil">
+        Ver Perfil
+      </Link>
+    </article>
+  );
+};
+
+export default TarjetaIntegrante;
