beeb467ebaef96b15778d5ed4947d2efa4aced26
leonardofvp <leonardovargasfp@gmail.com>
2026-05-31 13:05:16 -0300

    se corrige el impot de header.jsx

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import styles from "./Header.module.css";
-import BotonHamburguesa from "../../ui/Botones/botonHamburguesa";
+import BotonHamburguesa from "../../ui/Botones/BotonHamburguesa";
 
 function Header(props) {
   return (
